8dce395604dc4fc88241ec06b9e384b910c68528
221147cs18 <113594048+221147cs18@users.noreply.github.com>
2023-01-31 15:09:02 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  homework1
+//
+//  Created by KAMRONBEK UKTAMOV on 31/01/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Kamron" << "Uktamov" << "01";
+    
+          cout << "CS-23" << "/n";
+    
+    cout << "221147" << "/n";
+    
+    
+    
+    return 0;
+}
